d599badab5a30e0f6997a6746f29fdbb9e2d23b4
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-11 11:34:42 +0100

    Included first for in the js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,31 @@
 
-function main() {
-    console.log("Starting...");
-    const titulo=document.querySelector("h1");
-    titulo.addEventListener("pointerenter", imprimeHola);
-}
-
-window.addEventListener("load", main);
-
-
 function imprimeHola(){
     console.log("Hola");
 };
+
+
+
+function main() {
+    console.log("Starting...");
+    const titulo=document.querySelector("h1");
+    /*pointerenter: cuando se pasa el ratón por encima*/ 
+    titulo.addEventListener("pointerenter", imprimeHola);
+    const elements=Array.from(document.body.querySelectorAll("*"));
+
+    console.log(elements);
+
+    
+   for (let item of elements){
+       console.log(item);
+       item.addEventListener("pointerenter", ()=>{
+           
+        console.log("Nº de veces"+item)
+    });
+   }
+
+}
+
+/*load: Cuando la página esté cargada*/ 
+window.addEventListener("load", main);
+
+
